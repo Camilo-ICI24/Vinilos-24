d79ff57f41d95366ce0cad22b846dcb581e43459
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-08-14 09:43:28 -0400

    Actualización del código principal

--- src/Vinilos.java
+++ src/Vinilos.java
@@ -1,5 +1,12 @@
 package Vinilos;
 
 class coleccionDeVinilos {
-    public static void main
+    public static void main(String[] args) {
+        String informacion [][] = new String [100] [3];
+    }
+
+    public static String [][] agregarDisco(String m[][], String grupo, String nombre, String salida) {
+
+    }
 }
+
