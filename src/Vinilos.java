ec60f1599d70a3111597f0ffa90ef11a11899c9d
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-08-14 10:18:04 -0400

    Actualización del código principal

--- src/Vinilos.java
+++ src/Vinilos.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 class coleccionDeVinilos {
     public static void main(String[] args) {
         String informacion [][] = new String [100] [3];
+        agregarDisco(informacion)
+
     }
 
     public static String [][] agregarDisco(String m[][], String grupo, String nombre, String salida) {
         Scanner info = new Scanner(System.in);
         System.out.print("Ingrese la cantidad de discos que desea añadir: ");
         int cantidadASubir = info.nextInt();
-        for (int c > cantidadASubir) {
+        for (int i = 0; i < cantidadASubir ; i++) {
+            for (int fila = 0; fila = cantidadASubir - 1; fila++) {
+                for (int columna = 0; columna = 2; columna++) {
+                    System.out.print("Ingrese el grupo autor del disco: ");
+                    String grupo = info.nextLine();
+                    System.out.print()
+                }
+            }
+
 
         }
     }
